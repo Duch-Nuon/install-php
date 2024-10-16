aab7abc0e90ad7440484fad0e0b6f8a0e6a94b79
duchnuon <duchnuon@gmail.com>
2024-10-16 15:02:58 +0700

    updae

--- install_php.sh
+++ install_php.sh
@@ -7,7 +7,7 @@ sudo apt update
 sudo apt install -y software-properties-common
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
-sudo apt install -y php8.2 php8.2-fpm php8.2-mysql php8.2-xml php8.2-mbstring php8.2-curl php8.2-zip php8.2-bcmath php8.2-json php8.2-gd
+sudo apt install -y php8.2-fpm php8.2-mysql php8.2-xml php8.2-mbstring php8.2-curl php8.2-zip php8.2-bcmath php8.2-json php8.2-gd
 
 # Install MySQL Server
 sudo apt install -y mysql-server
@@ -15,9 +15,6 @@ sudo apt install -y mysql-server
 # Start MySQL service
 sudo systemctl start mysql
 sudo systemctl enable mysql
-
-# Check MySQL service status
-sudo service mysql status
 
 # Secure MySQL Installation
 sudo mysql_secure_installation
